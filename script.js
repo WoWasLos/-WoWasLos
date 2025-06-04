844b29345fd20a7ffcd046341f3b76649d81a1b5
WoWasLos <wowaslos7@gmail.com>
2025-06-04 14:01:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const supabaseUrl = 'https://bddofzmczzoiyausrdzb.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkZG9mem1jenpvaXlhdXNyZHpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMDQwMTIsImV4cCI6MjA2MzU4MDAxMn0.-MISfzyKIP3zUbJl5vOZDlUAGQXBqntbc9r_sG2zsJI';
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 // Leaflet-Karte initialisieren
 let map = L.map('map').setView([51.1657, 10.4515], 6);
